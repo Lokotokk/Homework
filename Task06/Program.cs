b773bd6661e33b66cd9938513ba0121462047da0
olya <abc@mail.ru>
2022-12-19 19:08:06 +0300

    add Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 6: Напишите программу, которая 
+на вход принимает число и 
+выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+Console.WriteLine("введите целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int remains = number1 % 2;
+if (remains == 0) Console.WriteLine($"число {number1} является четным");
+else Console.WriteLine($"число {number1} не является четным");
