07ab7c51e05a653010122ef0ef2aba9dfa48daad
olya <abc@mail.ru>
2022-12-21 15:09:21 +0300

    задача 11_домашнее задание к семинару 2

---
+++ Task11/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 10: Напишите программу, которая 
+принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа. 
+Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.WriteLine("введите трехзначное число:   ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 999 & number > 0)
+{
+    int secondDigit = SecondDigit(number);
+    Console.WriteLine($"{number} -> {secondDigit}");
+}
+else if (number < 0 & number >= -999)
+{
+    int secondDigit = SecondDigit(number);
+    int secondDigit2 = secondDigit * (-1);
+    Console.WriteLine($"{number} -> {secondDigit2}");
+}
+else
+{
+    Console.WriteLine("чило не является трехзначным");
+}
+int SecondDigit(int num)
+{
+    int remainder = num % 100;
+    return remainder / 10;
+}
