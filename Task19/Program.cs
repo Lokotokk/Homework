c31fbb5028b8b00b6a795b2440d8eeb5c43cd2c6
olya <abc@mail.ru>
2022-12-26 16:16:25 +0300

    Добавлена задача 19. ДЗ к семинару 3

---
+++ Task19/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 19.Напишите программу, которая 
+принимает на вход пятизначное число и 
+проверяет, является ли оно палиндромом.
+Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99999 || number < 10000) Console.WriteLine("число не является пятизначным");
+else
+{
+    bool fivedigitnumber = FiveDigitNumber(number);
+    Console.WriteLine(FiveDigitNumber(number) ? "-> да" : "-> нет");
+}
+bool FiveDigitNumber(int num)
+{
+    int remains = num % 10;
+    int remains2 = num / 1000 % 10;
+    return num / 10000 == remains && num % 100 / 10 == remains2;
+}
