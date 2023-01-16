733279b149deff4c4cded4fea4b70b9fd1b504f6
olya <abc@mail.ru>
2023-01-16 14:09:19 +0300

    Задача 27. ДЗ к семинару 4

---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.WriteLine("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 0) Console.WriteLine("число должно быть положительным");
+else Console.WriteLine($"{number}->{SumNumbers(number)}");
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
