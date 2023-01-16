54185feae887d5c7ce014afea6a770f4b230329f
olya <abc@mail.ru>
2023-01-16 14:52:54 +0300

    Задача 29. ДЗ к семинару 4

---
+++ Task29/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 29: Напишите программу, 
+которая задаёт массив из 8 элементов 
+и выводит их на экран.
+1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33];*/
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(" -> [{0}]", string.Join(", ", array));
+
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 100);
+    }
+}
+
+void PrintArray(int[] ar)
+{
+    for (int i = 0; i < ar.Length; i++)
+    {
+        Console.Write(ar[i]+", ");
+    }
+}
+
+
